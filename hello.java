c98f5cfb3ac1563a598e870c41001f7bd3e23b64
Sinchana12345CSE <129932812+Sinchana12345CSE@users.noreply.github.com>
2023-06-21 10:45:39 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hellosun
 	public static void main(String []args)
 	{
 		System.out.println("hello sun");
-		System.out.println("hello neptun");
+		System.out.println("hello neptun marduojkhgvf");
 	}
 }
 
