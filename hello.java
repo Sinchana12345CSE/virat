4009f49cdaf1bc58f86b62af37716b54ede61125
sanju <sanju@gmail.com>
2023-06-21 09:55:03 +0530

    change2

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@ public class hellosun
 {
 	public static void main(String []args)
 	{
-		System.out.println("hello sun")
+		System.out.println("hello sun");
+		System.out.println("hello moon");
 	}
 }
 
